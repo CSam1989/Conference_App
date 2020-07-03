d3ebefb264361054005a69b40db3121511ce2299
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-07-03 13:22:35 +0200

    Added Config (for app constants) file & IOC Container

---
+++ Presentation/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Presentation
+{
+    public class App
+    {
+        private readonly IConfiguration _config;
+
+        public App(
+            IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // Equivalent to Main in Program.cs
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Presentation
 {
-    class Program
+    // Found on https://github.com/Ballcapz/Console-Application-Dependency-Injection
+    internal class Program
     {
-        static void Main(string[] args)
+        // Will now be used for setup & IOC Container
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            services.AddTransient<App>();
+
+            return services;
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+
+            return builder.Build();
         }
     }
 }
