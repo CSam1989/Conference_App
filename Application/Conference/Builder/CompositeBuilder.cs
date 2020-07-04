7da62320b7e647ae7ee308ca5ac23cc35e3c38d8
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-07-04 16:30:33 +0200

    Added Builder Pattern

---
+++ Application/Conference/Builder/CompositeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Application.Conference.Composite;
+
+namespace Application.Conference.Builder
+{
+    public abstract class CompositeBuilder
+    {
+        private readonly IList<ConferenceComponent> _allTalks;
+
+        protected CompositeBuilder(IList<ConferenceComponent> allTalks)
+        {
+            _allTalks = allTalks;
+        }
+
+        public ConferenceClient BuildConference()
+        {
+            var conference = CreateComposite();
+
+            var remainingTalks = _allTalks;
+            var i = 1;
+
+            while (remainingTalks.Count != 0)
+            {
+                conference.Add(BuildTrack(ref remainingTalks, $"Track {i}:"));
+                i++;
+            }
+
+            return new ConferenceClient(conference);
+        }
+
+        public abstract ConferenceComponent CreateComposite(string name = null, int maxDuration = 0);
+        public abstract ConferenceComponent BuildTrack(ref IList<ConferenceComponent> remainingTalks, string name);
+    }
+}
---
+++ Application/Conference/Builder/ConferenceBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Application.Common.Interfaces;
+using Application.Common.Settings;
+using Application.Conference.Composite;
+
+namespace Application.Conference.Builder
+{
+    public class ConferenceBuilder : CompositeBuilder
+    {
+        private readonly IList<ConferenceComponent> _allTalks;
+        private readonly Sessions _sessions;
+        private readonly ITrackService _trackService;
+
+        public ConferenceBuilder(IList<ConferenceComponent> allTalks, ITrackService trackService, Sessions sessions) :
+            base(allTalks)
+        {
+            _allTalks = allTalks;
+            _trackService = trackService;
+            _sessions = sessions;
+        }
+
+        public override ConferenceComponent CreateComposite(string name = null, int maxDuration = 0)
+        {
+            return new ConferenceComposite(name, maxDuration);
+        }
+
+        public override ConferenceComponent BuildTrack(ref IList<ConferenceComponent> remainingTalks, string name)
+        {
+            var track = CreateComposite(name);
+
+            foreach (var sessionSettings in _sessions.SessionList)
+                track.Add(BuildSession(ref remainingTalks, sessionSettings.MaxLength));
+
+            return track;
+        }
+
+        private ConferenceComponent BuildSession(ref IList<ConferenceComponent> remainingTalks, int maxDuration)
+        {
+            var session = CreateComposite(null, maxDuration);
+
+            var sessionTalks =
+                _trackService.CalculateTalksForSession(remainingTalks, maxDuration);
+            remainingTalks = _trackService.RemoveSelectedTalksFromInputTalks(remainingTalks, sessionTalks);
+
+            foreach (var talk in sessionTalks) session.Add(talk);
+
+            return session;
+        }
+    }
+}
