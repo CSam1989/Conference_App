f8bd7c0c0a6f84138a5af1d1dfe2e72dd3352061
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-07-04 16:32:59 +0200

    Added Session constant variables

--- Application/Common/Settings/SessionSettings.cs
+++ Application/Common/Settings/SessionSettings.cs
@@ -1,6 +1,18 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Application.Common.Settings
 {
+    public class Sessions
+    {
+        public Sessions()
+        {
+            SessionList = new List<SessionSettings>();
+        }
+
+        public IList<SessionSettings> SessionList { get; set; }
+    }
+
     public class SessionSettings
     {
         public string Name { get; set; }
