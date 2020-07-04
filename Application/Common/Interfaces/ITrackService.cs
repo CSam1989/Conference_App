8c7e54da8f2000d9ab08fb978a2dfdd4408450d0
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-07-04 16:30:14 +0200

    Added Logic to calculate tracks

---
+++ Application/Common/Interfaces/ITrackService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Application.Conference.Composite;
+
+namespace Application.Common.Interfaces
+{
+    public interface ITrackService
+    {
+        IList<ConferenceComponent> CalculateTalksForSession
+            (IList<ConferenceComponent> allTalks, int maximumMinutes);
+
+        IList<ConferenceComponent> RemoveSelectedTalksFromInputTalks
+            (IList<ConferenceComponent> Talks, IList<ConferenceComponent> TalksToRemove);
+    }
+}
---
+++ Application/Common/Services/TrackService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Application.Common.Interfaces;
+using Application.Common.Settings;
+using Application.Conference.Composite;
+
+namespace Application.Common.Services
+{
+    public class TrackService : ITrackService
+    {
+        private readonly SpecialLengthSettings _specialLength;
+
+        public TrackService(SpecialLengthSettings specialLength)
+        {
+            _specialLength = specialLength;
+        }
+
+        public IList<ConferenceComponent> CalculateTalksForSession
+            (IList<ConferenceComponent> allTalks, int maximumMinutes)
+        {
+            var totalLength = 0;
+            var sessionTalks = new List<ConferenceComponent>();
+
+            foreach (var talk in allTalks)
+                if (totalLength + talk.Duration <= maximumMinutes)
+                {
+                    totalLength += talk.Duration;
+                    sessionTalks.Add(talk);
+                }
+
+            return sessionTalks;
+        }
+
+        public IList<ConferenceComponent> RemoveSelectedTalksFromInputTalks
+            (IList<ConferenceComponent> Talks, IList<ConferenceComponent> TalksToRemove)
+        {
+            foreach (var talk in TalksToRemove) Talks.Remove(talk);
+
+            return Talks;
+        }
+    }
+}
