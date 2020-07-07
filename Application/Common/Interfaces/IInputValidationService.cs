bf6d665e7bd47d91d170e7c8bf6ffc20bc726e5f
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-07-07 12:44:30 +0200

    Input validation service

---
+++ Application/Common/Interfaces/IInputValidationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IInputValidationService
+    {
+        bool IsValidTalkTitle(string name);
+        bool IsValidTalkDuration(string duration, out int output);
+    }
+}
